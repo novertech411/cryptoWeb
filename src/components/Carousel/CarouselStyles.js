2cbd2901182efeadfd62622387273c8bb99535fc
Oritsesholaye Eyinmosan <novertech4@gmail.com>
2022-04-08 09:30:07 -0700

    finished features task

---
+++ src/components/Carousel/CarouselStyles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import {Row} from '../../globalStyles';
+import {slider} from 'react-slick';
+
+export const CarouselImage = styled.img`
+width : 100%;
+height: 300px;
+border-radius: 10px 10px 00;
+object-fit: cover;
+vertical-align: middle;
+`
+
+export  const ImgWrapper = styled.div`
+width: 90%;
+display:flex;
+justify-content: center;
+align-items: center;
+flex-direction:column;
+border-radius: 10px;
+outline: none;
+height:430px;
+
+
+`
